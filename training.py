9b3a090e027d8b45a47307a13c0b506c54871c4f
Dimitris Katsiros <dkatsiros@gmail.com>
2020-06-22 19:26:42 +0300

    Improve training procedure

--- training.py
+++ training.py
@@ -2,24 +2,102 @@ import torch
 from torch.nn import functional as F
 import sys
 import math
-
+from copy import deepcopy
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score
+
+
+def train_and_validate(model,
+                       train_loader,
+                       valid_loader,
+                       loss_function,
+                       optimizer,
+                       epochs,
+                       cross_validation_epochs=5,
+                       early_stopping=False):
+    """
+    Trains the given <model>.
+    Then validates every <cross_validation_epochs>.
+    Returns: <best_model> containing the model with best parameters.
+    """
+
+    print(next(iter(train_loader)))
+
+    EPOCHS = epochs
+    CROSS_VALIDATION_EPOCHS = cross_validation_epochs
+    best_model = model
+
+    # Store losses, models
+    all_train_loss = []
+    all_valid_loss = []
+    models = []
+
+    # Iterate for EPOCHS
+    for epoch in range(1, EPOCHS + 1):
+
+        # ===== Training HERE =====
+        train_loss = train(epoch, train_loader, model,
+                           loss_function, optimizer)
+        # Store statistics for later usage
+        all_train_loss.append(train_loss)
+
+        # ====== VALIDATION HERE ======
+        if epoch % CROSS_VALIDATION_EPOCHS == 0:
+            valid_loss = validate(epoch, valid_loader,
+                                  model, loss_function)
+            # Store model
+            models.append(deepcopy(model))
+
+            # Store statistics for later usage
+            all_valid_loss.append(valid_loss)
+
+        # Make sure enough epochs have passed
+        if epoch < 4 * CROSS_VALIDATION_EPOCHS:
+            continue
+
+        # Remove first model when adding one new
+        models = models[1:-1]
+        best_model = model
+
+        # Early stopping enabled?
+        if early_stopping is False:
+            continue
+        # If enabled do everything needed
+        STOP = True
+        for i in range(2, 5):
+            if valid_loss < all_train_loss[-i]:
+                STOP = False
+                break
+        # Actually do early stopping
+        if STOP is True:
+            # But first save the model
+            idx = np.argmax(all_valid_loss)
+            best_model = models[idx]
+            break
+
+    return best_model, all_train_loss, all_valid_loss
 
 
 def train(_epoch, dataloader, model, loss_function, optimizer):
+    # Set model to train mode
     model.train()
-    running_loss = 0.0
+    training_loss = 0.0
 
     # obtain the model's device ID
     device = next(model.parameters()).device
 
+    # Iterate the batch
     for index, batch in enumerate(dataloader, 1):
 
-        # get the inputs (batch)
-        inputs, labels, lengths = batch
+        # Split the contents of each batch[i]
+        try:
+            # LSTM
+            inputs, labels, lengths = batch
+        except ValueError:
+            # CNN
+            inputs, labels = batch
 
         inputs = inputs.to(device)
         labels = labels.to(device)
@@ -28,9 +106,12 @@ def train(_epoch, dataloader, model, loss_function, optimizer):
         optimizer.zero_grad()
 
         # Forward pass: y' = model(x)
-        y_preds = model.forward(inputs, lengths)
+        try:
+            y_preds = model.forward(inputs, lengths)
+        except:
+            y_preds = model.forward(inputs)
 
-        # Compute loss: L = loss_function(y, y')
+        # Compute loss: L = loss_function(y', y)
         loss = loss_function(y_preds, labels)
 
         # Backward pass: compute gradient wrt model parameters
@@ -39,7 +120,8 @@ def train(_epoch, dataloader, model, loss_function, optimizer):
         # Update weights
         optimizer.step()
 
-        running_loss += loss.data.item()
+        # Add loss to total epoch loss
+        training_loss += loss.data.item()
 
         # print statistics
         progress(loss=loss.data.item(),
@@ -48,20 +130,90 @@ def train(_epoch, dataloader, model, loss_function, optimizer):
                  batch_size=dataloader.batch_size,
                  dataset_size=len(dataloader.dataset))
 
-    return running_loss / index
+    # Return loss
+    return training_loss / len(dataloader)
 
 
-def validate(dataloader, model, criterion, loss_function):
+def validate(_epoch, dataloader, model, loss_function):
+    """Validate the model."""
+
+    # Put model to evalutation mode
+    model.eval()
+
     with torch.no_grad():
         valid_loss = 0
-        for index, batch in enumerate(1, dataloader):
+
+        for index, batch in enumerate(dataloader, 1):
+
             # Get the sample
             inputs, labels, lengths = batch
+
             # Forward through the network
             y_pred = model.forward(inputs, lengths)
-            # Add validation loss
-            valid_loss += loss_function(y_pred, labels.type('torch.LongTensor'))
 
+            # Compute loss
+            loss = loss_function(y_pred, labels.type('torch.LongTensor'))
+
+            # Add validation loss
+            valid_loss += loss.data.item()
+
+        # Print some stats
+        print(f'\nValidation loss at epoch {_epoch} : {round(valid_loss, 4)}')
+
+    return valid_loss / len(dataloader)
+
+
+def test(model, dataloader):
+    """
+    Tests a given model.
+    Returns an array with predictions and an array with labels.
+    """
+
+    correct = 0
+    # Create empty array for storing predictions and labels
+    y_pred = []
+    y_true = []
+    for index, batch in enumerate(dataloader, 1):
+        # Split each batch[index]
+        inputs, labels, lengths = batch
+
+        # Forward through the network
+        out = model.forward(inputs, lengths)
+
+        # Predict the one with the maximum probability
+        predictions = F.softmax(out, dim=-1).argmax(dim=-1)
+
+        # Save predictions
+        y_pred.append(predictions.data.numpy())
+        y_true.append(labels.data.numpy())
+
+    # Get metrics
+    y_pred = np.array(y_pred).flatten()
+    y_true = np.array(y_true).flatten()
+
+    return y_pred, y_true
+
+
+def results(model, train_loss, valid_loss, y_pred, y_true, epochs, cv=5):
+    """Prints the results of training. Also saves some plots."""
+    # Plots
+    fig = plt.figure(figsize=(10, 10))
+    plt.ylabel('Train - Validation Loss')
+    plt.xlabel('Epoch')
+    try:
+        plt.plot(list(range(1, epochs + 1)), train_loss, color='r')
+        valid_loss_for_plot = np.array([[l]*cv for l in valid_loss]).flatten()
+        plt.plot(list(range(1, epochs + 1)), valid_loss_for_plot, color='b')
+        plt.savefig(
+            f'plotting/plots/{model.__class__.__name__}_train_valid_loss.png')
+    except Exception as e:
+        print('\Exception raised while creating plot: {e}')
+
+    # Print metrics
+    f1_metric = f1_score(y_true, y_pred, average='macro')
+    print(f'f1 score: {round(f1_metric,4)}')
+    acc = accuracy_score(y_true, y_pred)
+    print(f'Accuracy: {round(acc,4)}')
 
 
 def progress(loss, epoch, batch, batch_size, dataset_size):
@@ -82,131 +234,4 @@ def progress(loss, epoch, batch, batch_size, dataset_size):
 
     if batch == batches:
         print()
-
-
-def fit(model, epochs, lr, loader, v_loader, L2=0, valTime=5, showVal=True, earlyStopping=False):
-    """Train the model and also validate."""
-
-    # Define loss function.
-    loss_func = torch.nn.CrossEntropyLoss()
-    # All loss
-    train_loss_per_epoch = []
-    valid_loss_per_epoch = []
-    # Use Stochastic Gradient Descent of the optim package for parameter updates.
-    # opt = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=L2)
-    opt = torch.optim.Adadelta(
-        model.parameters(), lr=lr, rho=0.9, eps=1e-06, weight_decay=L2)
-
-
-    # A flag  for validation loss.
-    old_valid_loss = -62
-    old_net = model
-    for epoch in range(epochs):
-
-        ## TRAINING ---------------------
-        # Initialize loss
-        epoch_loss = 0
-        # It is wise, according to the documentation, to put the network in
-        # training mode before each batch is loaded.
-        model.train()
-        # iterate for inputs, labels, lengths
-        for xb, yb, lb in loader:
-            # Predict
-            out = model.forward(xb, lb)
-            # Get loss
-            loss = loss_func(out, yb.type('torch.LongTensor'))
-            # Add loss to current epoch
-            epoch_loss += loss
-            # Backpropagate the loss.
-            loss.backward()
-            # Update weights, bias.
-            opt.step()
-            # A new batch is ready to be loaded. Clean gradient memory!
-            opt.zero_grad()
-        print('Train loss at epoch', epoch, ':', float(epoch_loss))
-        # Also store
-        train_loss_per_epoch.append(float(epoch_loss))
-
-
-        ## VALIDATION ---------------------------
-        # At the end of each epoch, the network is put in evaluation mode.
-        model.eval()
-        # Will infer on the validation set (and maybe check for early       \
-        # stopping) every earlyCheck epochs.
-        if showVal and epoch % valTime == 0:
-            # No reason to keep the gradient for the validation set.
-            with torch.no_grad():
-                valid_loss = 0
-                for xb, yb, lb in v_loader:
-                    out = model.forward(xb, lb)#.view(1, -1)
-                    valid_loss += loss_func(out, yb.type('torch.LongTensor'))
-                print('Validation loss at epoch',
-                      epoch, ':', float(valid_loss))
-
-                valid_loss_per_epoch.append(float(valid_loss))
-                # Early stopping!
-                if (earlyStopping and epoch >= 20 and
-                    valid_loss_per_epoch[-1] > valid_loss_per_epoch[-2] and
-                    valid_loss_per_epoch[-1] > valid_loss_per_epoch[-3] and
-                    valid_loss_per_epoch[-1] > valid_loss_per_epoch[-4]):
-                    # if the criterion is broken, pause training and return the
-                    # previous logged net
-                    model = old_net
-                    print(
-                        'Training finished due to early stopping.\n Actual number of epochs:', epoch)
-                    break
-                old_valid_loss = valid_loss
-                # If the validation has passed, keep the net logged!
-                old_net = model
-    return model, train_loss_per_epoch, valid_loss_per_epoch
-
-
-def print_results(net, val_loader, test_loader, train_loss, valid_loss, epochs):
-    """Some results needed for evaluation."""
-
-    val_conf = np.zeros((7, 7))
-    for xb, yb, lb in val_loader:
-        out = net.forward(xb, lb)
-        preds = F.softmax(out, dim=-1).argmax(dim=-1)
-        acc_val = int((preds == yb).sum()) / len(yb) * 100
-    print('Accuracy on validation set:', acc_val, '%')
-
-    # Testing
-    predictions = []
-    labels = []
-    for xb, yb, lb in test_loader:
-        # Pass the test data through the network
-        out = net.forward(xb, lb)
-        # Predict the one with the highest probability
-        preds = F.softmax(out, dim=-1).argmax(dim=-1)
-        # Need for metrics
-        predictions.append(preds.data.numpy())
-        labels.append(yb.data.numpy())
-        # Get accuracy manually
-        acc_test = int((preds == yb).sum()) / len(yb) * 100
-
-    print('Accuracy on test set:', acc_test, '%')
-
-    # Train and validation
-    fig = plt.figure(figsize=(10,10))
-    plt.ylabel('Train-Validation loss')
-    plt.xlabel('Epoch')
-    try:
-        plt.plot(list(range(1, epochs+1)), train_loss_per_epoch, color='r')
-    except:
-        print("Train loss can't be printed")
-    try:
-        # Convert valid losses to printable
-        vls = np.array([[l]*5 for l in valid_loss_per_epoch]).flatten()
-        plt.plot(list(range(len(vls))), vls, color='b')
-    except:
-        print("Validation loss can't be printed")
-    plt.savefig('plotting/plots/train_valid_loss.png')
-
-    # Print some metrics
-    predictions = np.array(predictions).flatten()
-    labels = np.array(labels).flatten()
-    f1_metric = f1_score(labels, predictions, average='macro')
-    print(f'f1 score: {f1_metric}')
-
 
