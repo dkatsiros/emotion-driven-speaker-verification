Fix model return values, removing double softmax
CrossEntropyLoss in pytorch takes values and appies Softmax itself

dataloading.py
@@ -19,7 +19,8 @@ class EmodbDataset(Dataset):
             feature_extraction_method {string} -- [What method extracts the features]
             oversampling {bool} -- [Resampling technique to be applied]
         """
-        self.max_length = 281
+        self.feature_extraction_method = feature_extraction_method
+
         if oversampling is True:
             ros = RandomOverSampler()
             # Expand last dimension
@@ -56,6 +57,7 @@ class EmodbDataset(Dataset):
         # Get all lengths before zero padding
         lengths = np.array([len(x) for x in X])
         self.lengths = torch.tensor(lengths)
+
         # Zero pad all samples
         X = self.zero_pad_and_stack(X)
         # Create tensor for features
@@ -81,15 +83,20 @@ class EmodbDataset(Dataset):
         # print(self.X[index].size(), self.y[index].size(), self.lengths[index].size())
         return self.X[index], self.y[index], self.lengths[index]
 
-    def zero_pad_and_stack(self, X):
+    def zero_pad_and_stack(self, X,):
         """
         This function performs zero padding on a list of features and forms them into a numpy 3D array
 
         Returns:
             padded: a 3D numpy array of shape num_sequences x max_sequence_length x feature_dimension
         """
+        if self.feature_extraction_method == "MFCC":
+            max_length = self.lengths.max()
+        elif self.feature_extraction_method == "MEL_SPECTROGRAM":
+            max_length = 281  # self.lengths.max()
+        else:
+            raise AssertionError()
 
-        max_length = self.max_length  # self.lengths.max()
         feature_dim = X[0].shape[-1]
         padded = np.zeros((len(X), max_length, feature_dim))
 

main.py
@@ -10,21 +10,23 @@ from torch.nn import functional as F
 
 from models.lstm import LSTM
 from models.cnn import CNN
-from training import train_and_validate, test, results
+from training import train_and_validate, test, results, overfit
 # progress, fit, print_results
 from config import VARIABLES_FOLDER, RECOMPUTE
 import os
 import joblib
 
+from plotting.class_stats import dataloader_stats
+
 # Split dataset to arrays
 X_train, y_train, X_test, y_test, X_eval, y_eval = load_Emodb()
 
 # PyTorch
-BATCH_SIZE = len(X_train) // 20
+BATCH_SIZE = 16  # len(X_train) // 20
 print(f'Selected Batch Size: {BATCH_SIZE}')
 EPOCHS = 500
 
-CNN_BOOLEAN = True
+CNN_BOOLEAN = False
 
 # Load sets using dataset class
 train_set = EmodbDataset(X_train, y_train, oversampling=True,
@@ -43,8 +45,10 @@ try:
     train_loader = joblib.load(TRAIN_LOADER)
 except:
     train_loader = DataLoader(
-        train_set, batch_size=BATCH_SIZE, num_workers=4, drop_last=True)
+        train_set, batch_size=BATCH_SIZE, num_workers=4, drop_last=True, shuffle=True)
     joblib.dump(train_loader, TRAIN_LOADER)
+dataloader_stats(train_loader, filename='train_loader_statistics.png')
+
 try:
     TEST_LOADER = os.path.join(VARIABLES_FOLDER, 'test_loader.pkl')
     if RECOMPUTE is True:
@@ -52,8 +56,9 @@ try:
     test_loader = joblib.load(TEST_LOADER)
 except:
     test_loader = DataLoader(
-        test_set, batch_size=BATCH_SIZE, num_workers=4, drop_last=True)
+        test_set, batch_size=BATCH_SIZE, num_workers=4, drop_last=True, shuffle=True)
     joblib.dump(test_loader, TEST_LOADER)
+    dataloader_stats(test_loader, filename='test_loader_statistics.png')
 
 try:
     VALID_LOADER = os.path.join(VARIABLES_FOLDER, 'valid_loader.pkl')
@@ -62,18 +67,19 @@ try:
     valid_loader = joblib.load(VALID_LOADER)
 except:
     valid_loader = DataLoader(
-        eval_set, batch_size=BATCH_SIZE, num_workers=4, drop_last=True)
+        eval_set, batch_size=BATCH_SIZE, num_workers=4, drop_last=True, shuffle=True)
     joblib.dump(valid_loader, VALID_LOADER)
+    dataloader_stats(valid_loader, filename='valid_loader_statistics.png')
 
 # if your computer has a CUDA compatible gpu use it, otherwise use the cpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f'Running on: {device}.\n')
 
 # Create a model
-# model = LSTM(input_size=39, hidden_size=16, output_size=7, num_layers=1,
-#  bidirectional=False, dropout=0.1)
+model = LSTM(input_size=39, hidden_size=16, output_size=7, num_layers=1,
+             bidirectional=False, dropout=0.4)
 
-model = CNN(output_dim=7)
+# model = CNN(output_dim=7)
 print(f'Model Parameters: {model.count_parameters(model)}')
 
 # move model weights to device
@@ -87,23 +93,34 @@ print(model)
 
 print(next(iter(train_loader)))
 # Regularization parameters
-learning_rate = 1
+learning_rate = 1e-5
 L2 = 0
 # Loss and optimizer
 loss_function = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adadelta(
-    model.parameters(), lr=learning_rate, rho=0.9, eps=1e-06, weight_decay=L2)
+# optimizer = torch.optim.Adadelta(
+#     model.parameters(), rho=0.9, weight_decay=L2)  # eps = 1e-06
+# optimizer = torch.optim.SGD(params=model.parameters(
+# ), lr=learning_rate, momentum=0.9, weight_decay=L2)
+optimizer = torch.optim.Adam(model.parameters())
 CROSS_VALIDATION_EPOCHS = 5
 
-best_model, train_losses, valid_losses, _epochs = train_and_validate(model=model,
-                                                                     train_loader=train_loader,
-                                                                     valid_loader=valid_loader,
-                                                                     loss_function=loss_function,
-                                                                     optimizer=optimizer,
-                                                                     epochs=EPOCHS,
-                                                                     cnn=CNN_BOOLEAN,
-                                                                     cross_validation_epochs=5,
-                                                                     early_stopping=True)
+# Test overfit
+# model, all_train_loss, epoch = overfit(model,
+#                                       train_loader,
+#                                       loss_function,
+#                                       optimizer,
+#                                       epochs=EPOCHS,
+#                                       cnn=CNN_BOOLEAN)
+# exit()
+best_model, train_losses, valid_losses, train_accuracy, _epochs = train_and_validate(model=model,
+                                                                                     train_loader=train_loader,
+                                                                                     valid_loader=valid_loader,
+                                                                                     loss_function=loss_function,
+                                                                                     optimizer=optimizer,
+                                                                                     epochs=EPOCHS,
+                                                                                     cnn=CNN_BOOLEAN,
+                                                                                     cross_validation_epochs=5,
+                                                                                     early_stopping=True)
 
 timestamp = time.ctime()
 
@@ -114,5 +131,7 @@ joblib.dump(best_model, modelname)
 # ===== TEST =====
 y_pred, y_true = test(best_model, test_loader, cnn=CNN_BOOLEAN)
 # ===== RESULTS =====
-results(model=best_model, train_loss=train_losses, valid_loss=valid_losses,
-        y_pred=y_pred, y_true=y_true, epochs=_epochs, cv=CROSS_VALIDATION_EPOCHS, timestamp=timestamp)
+results(model=best_model, optimizer=optimizer, loss_function=loss_function,
+        train_loss=train_losses, valid_loss=valid_losses, train_accuracy=train_accuracy,
+        y_pred=y_pred, y_true=y_true, epochs=_epochs,
+        cv=CROSS_VALIDATION_EPOCHS, timestamp=timestamp)

models/cnn.py
@@ -24,16 +24,19 @@ class CNN(nn.Module):
         self.conv2 = nn.Conv2d(10, 20, kernel_size=3)
         self.conv3 = nn.Conv2d(20, 40, kernel_size=3)
         self.conv4 = nn.Conv2d(40, 80, kernel_size=3)
+        # self.conv5 = nn.Conv2d(80, 160, kernel_size=3)
 
         self.conv1_bn = nn.BatchNorm2d(10)  # batch normalization
         self.conv2_bn = nn.BatchNorm2d(20)
         self.conv3_bn = nn.BatchNorm2d(40)
         self.conv4_bn = nn.BatchNorm2d(80)
+        # self.conv5_bn = nn.BatchNorm2d(160)
 
         self.dropout1 = nn.Dropout2d(0.25)  # dropout
         self.dropout2 = nn.Dropout2d(0.5)
 
-        self.fc1 = nn.Linear(7200, 4096)   # linear layer
+        # self.fc0 = nn.Linear(18480, 7200)
+        self.fc1 = nn.Linear(7200, 4096)   # linear layer 7200
         self.fc2 = nn.Linear(4096, 1024)
         self.fc3 = nn.Linear(1024, 256)
         self.fc4 = nn.Linear(256, output_dim)
@@ -58,12 +61,17 @@ class CNN(nn.Module):
 
         x = F.max_pool2d(F.relu(self.conv4_bn(self.conv4(x))),
                          2)  # [14, 80, 13, 6]
+
+        # x = F.max_pool2d(F.relu(self.conv5_bn(self.conv5(x))),
+        #                  2)  # [14,160,5,2]
         # print('Conv4:', np.shape(x))
 
         # batch_size x new_dim(ginomeno ton allon)
-        x = F.relu(self.fc1(x.view(len(x), -1)))
-        x = self.dropout2(F.relu(self.fc2(x)))
-        x = self.dropout2(F.relu(self.fc3(x)))
+        x = F.relu(self.fc1(x.view(len(x), - 1)))
+        # x = F.relu(self.fc1(x.view(len(x), -1)))
+        # x = self.dropout1(F.relu(self.fc1(x)))
+        x = self.dropout1(F.relu(self.fc2(x)))
+        x = self.dropout1(F.relu(self.fc3(x)))
         x = F.relu(self.fc4(x))
 
         if self.multitask:
@@ -71,10 +79,11 @@ class CNN(nn.Module):
         elif self.regression:
             x = x.view(-1)
         else:
-            x = F.log_softmax(x, dim=1)
+            # x = F.log_softmax(x)  # , dim=1)
+            return x
 
         return x
 
-    @staticmethod
+    @ staticmethod
     def count_parameters(model):
         return sum(p.numel() for p in model.parameters() if p.requires_grad)

models/lstm.py
@@ -2,11 +2,12 @@ import torch
 from torch import nn
 import numpy as np
 
+
 class LSTM(nn.Module):
     """LSTM implementation."""
 
     def __init__(self, input_size, hidden_size=16, output_size=7, num_layers=1,
-                bidirectional=False, dropout=0):
+                 bidirectional=False, dropout=0):
 
         super(LSTM, self).__init__()
 
@@ -25,7 +26,6 @@ class LSTM(nn.Module):
         # Final layer
         self.linear = nn.Linear(self.hidden_size, output_size)
         self.softmax = nn.Softmax()
-
 
     def forward(self, x, lengths):
         """
@@ -62,11 +62,11 @@ class LSTM(nn.Module):
 
         # Pass the output of LSTM to the remaining network, projecting to classes
         representations = self.linear(last)
+        return representations
         # Convert to probabilities
         logits = self.softmax(representations)
 
         return logits
-
 
     @staticmethod
     def last_by_index(outputs, lengths):
@@ -74,7 +74,6 @@ class LSTM(nn.Module):
         idx = (lengths - 1).view(-1, 1).expand(outputs.size(0),
                                                outputs.size(2)).unsqueeze(1)
         return outputs.gather(1, idx).squeeze()
-
 
     @staticmethod
     def count_parameters(model):

training.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import functional as F
+import os
 import sys
 import math
 from copy import deepcopy
@@ -12,6 +13,7 @@ from sklearn.metrics import confusion_matrix
 from utils.emodb import get_classes
 from sklearn.metrics import f1_score, accuracy_score
 from plotting.metrics import plot_confusion_matrix
+from config import PLOTS_FOLDER, REPORTS_FOLDER
 
 
 def train_and_validate(model,
@@ -39,6 +41,7 @@ def train_and_validate(model,
     best_model = model
 
     # Store losses, models
+    all_accuracy_training = []
     all_train_loss = []
     all_valid_loss = []
     models = []
@@ -47,10 +50,11 @@ def train_and_validate(model,
     for epoch in range(1, EPOCHS + 1):
 
         # ===== Training HERE =====
-        train_loss = train(epoch, train_loader, model,
-                           loss_function, optimizer, cnn=cnn)
+        train_loss, train_acc = train(epoch, train_loader, model,
+                                      loss_function, optimizer, cnn=cnn)
         # Store statistics for later usage
         all_train_loss.append(train_loss)
+        all_accuracy_training.append(train_acc)
 
         # ====== VALIDATION HERE ======
         if epoch % CROSS_VALIDATION_EPOCHS == 0:
@@ -76,25 +80,33 @@ def train_and_validate(model,
         # If enabled do everything needed
         STOP = True
         # Check last 4 validations
-        for i in range(1, 4):
+        for i in range(1, 3):
             # If at least one val_loss_new < val_loss_older dont stop
             if all_valid_loss[-i] < all_valid_loss[-i-1]:
                 STOP = False
                 break
+        # Small change in loss
+        if (abs(all_valid_loss[-1] - all_valid_loss[-2]) < 0.01 and
+                abs(all_valid_loss[-2] - all_valid_loss[-3]) < 0.01):
+            STOP = False
+            break
+
         # Actually do early stopping
         if STOP is True:
             # Reset last model
             print('Resetting to previous model! ..\n')
-            best_model = models[-3].to(device)
+            best_model = models[-2].to(device)
             break
 
-    return best_model, all_train_loss, all_valid_loss, epoch
+    return best_model, all_train_loss, all_valid_loss, all_accuracy_training, epoch
 
 
 def train(_epoch, dataloader, model, loss_function, optimizer, cnn=False):
     # Set model to train mode
     model.train()
     training_loss = 0.0
+    correct = 0
+    total = 0
 
     # obtain the model's device ID
     device = next(model.parameters()).device
@@ -106,7 +118,7 @@ def train(_epoch, dataloader, model, loss_function, optimizer, cnn=False):
         inputs, labels, lengths = batch
 
         inputs = inputs.to(device)
-        labels = labels.to(device)
+        labels = labels.type('torch.LongTensor').to(device)
 
         # Clear gradients
         optimizer.zero_grad()
@@ -120,9 +132,17 @@ def train(_epoch, dataloader, model, loss_function, optimizer, cnn=False):
             inputs = inputs[:, np.newaxis, :, :]
             y_preds = model.forward(inputs)
 
+        # print(f'\ny_preds={y_preds}')
+        # print(f'\nlabels={labels}')
         # Compute loss: L = loss_function(y', y)
         loss = loss_function(y_preds, labels)
 
+        labels_cpu = labels.detach().clone().to('cpu').numpy()
+        # Get accuracy
+        correct += sum([int(a == b)
+                        for a, b in zip(labels_cpu,
+                                        np.argmax(y_preds.detach().clone().to('cpu').numpy(), axis=1))])
+        total += len(labels_cpu)
         # Backward pass: compute gradient wrt model parameters
         loss.backward()
 
@@ -139,8 +159,19 @@ def train(_epoch, dataloader, model, loss_function, optimizer, cnn=False):
                  batch_size=dataloader.batch_size,
                  dataset_size=len(dataloader.dataset))
 
-    # Return loss
-    return training_loss / len(dataloader)
+    # print statistics
+    progress(loss=training_loss/len(dataloader),
+             epoch=_epoch,
+             batch=index,
+             batch_size=dataloader.batch_size,
+             dataset_size=len(dataloader.dataset))
+
+    accuracy = correct/total * 100
+    # Print some stats
+    # print(
+    #     f'\nTrain loss at epoch {_epoch} : {round(training_loss/len(dataloader), 4)}')
+    # Return loss, accuracy
+    return training_loss / len(dataloader), accuracy
 
 
 def validate(_epoch, dataloader, model, loss_function, cnn=False):
@@ -161,7 +192,7 @@ def validate(_epoch, dataloader, model, loss_function, cnn=False):
             inputs, labels, lengths = batch
             # Transfer to device
             inputs = inputs.to(device)
-            labels = labels.to(device)
+            labels = labels.type('torch.LongTensor').to(device)
 
             # Forward through the network
             if cnn is False:
@@ -202,7 +233,8 @@ def test(model, dataloader, cnn=False):
 
         # Transfer to device
         inputs = inputs.to(device)
-        labels = labels.to(device)
+        labels = labels.type('torch.LongTensor').to(device)
+        # print(f'\n labels: {labels}')
 
         # Forward through the network
         if cnn is False:
@@ -212,8 +244,10 @@ def test(model, dataloader, cnn=False):
             inputs = inputs[:, np.newaxis, :, :]
             out = model.forward(inputs)
         # Predict the one with the maximum probability
-        predictions = F.softmax(out, dim=-1).argmax(dim=-1)
-
+        # predictions = F.softmax(out, dim=-1).argmax(dim=-1)
+        # print(f'\nout:{out}')
+        predictions = torch.argmax(out, -1)
+        # print(f'\nprediction: {predictions}')
         # Save predictions
         y_pred.append(predictions.cpu().data.numpy())
         y_true.append(labels.cpu().data.numpy())
@@ -225,35 +259,15 @@ def test(model, dataloader, cnn=False):
     return y_pred, y_true
 
 
-def results(model, train_loss, valid_loss, y_pred, y_true, epochs, cv=5):
+def results(model, optimizer, loss_function, train_loss, valid_loss, train_accuracy, y_pred, y_true, epochs, timestamp, cv=5):
     """Prints the results of training. Also saves some plots."""
     # Plots
-    fig = plt.figure(figsize=(10, 10))
-    plt.ylabel('Train - Validation Loss')
-    plt.xlabel('Epoch')
+
+    # Train validation plot
     try:
-        plt.plot(list(range(1, epochs + 1)), train_loss, color='r')
-        valid_loss_for_plot = np.array([[l]*cv for l in valid_loss]).flatten()
-        plt.plot(list(range(1, epochs + 1)), valid_loss_for_plot, color='b')
-        plt.savefig(
-            f'plotting/plots/{model.__class__.__name__}_train_valid_loss.png')
-    except Exception as e:
-        print('\Exception raised while creating plot: {e}')
-
-    # Print metrics
-    f1_metric = f1_score(y_true, y_pred, average='macro')
-    print(f'f1 score: {round(f1_metric,4)}')
-    acc = accuracy_score(y_true, y_pred)
-    print(f'Accuracy: {round(acc,4)}')
-
-
-def results(model, train_loss, valid_loss, y_pred, y_true, epochs, timestamp, cv=5):
-    """Prints the results of training. Also saves some plots."""
-    # Plots
-    fig = plt.figure(figsize=(10, 10))
-    plt.ylabel('Train - Validation Loss')
-    plt.xlabel('Epoch')
-    try:
+        fig = plt.figure(figsize=(10, 10))
+        plt.ylabel('Train - Validation Loss')
+        plt.xlabel('Epoch')
         plt.plot(list(range(1, epochs + 1)), train_loss,
                  color='r', label='Training')
         vld_values = np.array([[l]*cv for l in valid_loss]).flatten()
@@ -265,13 +279,30 @@ def results(model, train_loss, valid_loss, y_pred, y_true, epochs, timestamp, cv
                  color='b', label='Validation')
         plt.legend()
         plot_filename = f'train_valid_loss_{model.__class__.__name__}_{epochs}_{timestamp}.png'
-        plt.savefig(
-            f'plotting/plots/{plot_filename}')
+        plt.savefig(os.path.join(PLOTS_FOLDER, plot_filename))
     except Exception as e:
         print(f'Exception raised while creating plot: {e}')
     finally:
         # Just in case plotting fails. We need the name below
         plot_filename = f'train_valid_loss_{model.__class__.__name__}_{epochs}_{timestamp}.png'
+
+    #  Accuracy plot
+    try:
+        plt.figure(figsize=(10, 10))
+        plt.plot(list(range(1, epochs + 1)), train_accuracy,
+                 color='r', label='Accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.title('Training Accuracy')
+        plt.legend()
+        accuracy_filename = f'accuracy_{model.__class__.__name__}_{epochs}_{timestamp}.png'
+        plt.savefig(os.path.join(PLOTS_FOLDER, accuracy_filename))
+
+    except Exception as e:
+        print(f'Exception raised while creating plot: {e}')
+    finally:
+        # Just in case plotting fails. We need the name below
+        accuracy_filename = f'accuracy_{model.__class__.__name__}_{epochs}_{timestamp}.png'
 
     # Print metrics
     f1_metric = f1_score(y_true, y_pred, average='macro')
@@ -289,16 +320,34 @@ def results(model, train_loss, valid_loss, y_pred, y_true, epochs, timestamp, cv
         y_true=y_true, y_pred=y_pred, target_names=classes)
     # Save metrics
     filename = f'{model.__class__.__name__}_{epochs}_{timestamp}.md'
-    with open(f'plotting/reports/{filename}', mode='w') as f:
+    with open(os.path.join(REPORTS_FOLDER, filename), mode='w') as f:
         data = f"""
 # REPORT:
-### {filename}
+# {filename}
+## Model details:
+```python
+{model}
+```
+## Optimizer
+```python
+{optimizer}
+```
+## Loss function
+```python
+{loss_function}
+```
+## Metrics:
 f1-macro-score = {f1_metric}
 acc = {acc}
+## Classification Report:
 ```
 {model_classification_report}
 ```
+## Training / Validation Loss
 <img src='../plots/{plot_filename}'>
+## Training Accuracy
+<img src='../plots/{accuracy_filename}'>
+## Confusion matrix
 <img src='../plots/{cnf_mtrx_filename}'>
     """
         f.write(data)
@@ -322,3 +371,40 @@ def progress(loss, epoch, batch, batch_size, dataset_size):
 
     if batch == batches:
         print()
+
+
+def overfit(model,
+            train_loader,
+            loss_function,
+            optimizer,
+            epochs,
+            cnn=False):
+    """
+    Trains the given <model>.
+    Then validates every <cross_validation_epochs>.
+    Returns: <best_model> containing the model with best parameters.
+    """
+
+    # obtain the model's device ID
+    device = next(model.parameters()).device
+
+    print(next(iter(train_loader)))
+
+    EPOCHS = epochs
+
+    # Store losses, models
+    all_train_loss = []
+    models = []
+
+    # Iterate for EPOCHS
+    for epoch in range(1, EPOCHS + 1):
+
+        # ===== Training HERE =====
+        train_loss = train(epoch, train_loader, model,
+                           loss_function, optimizer, cnn=cnn)
+        # Store statistics for later usage
+        all_train_loss.append(train_loss)
+        if epoch % 5 == 0:
+            print(f'\nEpoch {epoch} loss: {train_loss}')
+
+    return model, all_train_loss, epoch