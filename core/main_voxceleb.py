74efa3f931cf243b137ebb48c43db58a853e1c8e
Dimitris Katsiros <dkatsiros@gmail.com>
2020-11-15 15:38:56 +0200

    Add dataset function in test

--- core/main_voxceleb.py
+++ core/main_voxceleb.py
@@ -285,12 +285,12 @@ def test_voxceleb(max_seq_len=245):
     _, test_speakers, _ = load_VoxCeleb(validation=False)
 
     fe_method = "MEL_SPECTROGRAM" if config.CNN_BOOLEAN is True else "MFCC"
-
+    dataset_func = Voxceleb1PreComputedMelSpectr if config.PRECOMPUTED_MELS is True else Voxceleb1
     # Test dataloader
-    test_dataset = Voxceleb1(X=test_speakers,
-                             test=True,
-                             fe_method=fe_method,
-                             max_seq_len=max_seq_len)
+    test_dataset = dataset_func(X=test_speakers,
+                                test=True,
+                                fe_method=fe_method,
+                                max_seq_len=max_seq_len)
     test_loader = DataLoader(test_dataset, batch_size=config.SPEAKER_N,
                              shuffle=False, num_workers=config.NUM_WORKERS, drop_last=True)
 
