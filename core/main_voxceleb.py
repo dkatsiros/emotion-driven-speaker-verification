Add EER computation as library, minor fixes

core/main_voxceleb.py
@@ -8,12 +8,14 @@ import logging
 # External imports
 import torch
 from torch.utils.data import DataLoader
+import numpy as np
 import glob2 as glob
 from tqdm import tqdm
 
 # Relative imports
 import config
 from config import VARIABLES_FOLDER, RECOMPUTE, DETERMINISTIC
+from lib.metrics import tuneThresholdfromScore
 from lib.loss import GE2ELoss
 from lib.model_editing import drop_layers, print_require_grad_parameter
 from lib.sound_processing import compute_max_sequence_length, compute_sequence_length_distribution
@@ -107,69 +109,59 @@ def test_se(model, dataloader, testing_epochs=10):
     # obtain the model's device ID
     device = next(model.parameters()).device
 
-    avg_EER = 0
-    all_cossim = []
+    avg_EER = []
+    avg_EER_ = []
     for e in range(testing_epochs):
         batch_avg_EER = 0
+        batch_avg_EER_ = 0
         # dataloader: (batch_size,2,max_seq_len,input_fe)
         for batch_id, (batch, labels) in enumerate(tqdm(dataloader)):
             # move to device
-            batch = batch.to(device)
+            batch = batch.to(device, dtype=torch.float)
             labels = labels.to(device)
             # prepare for forward pass
             batch = torch.reshape(batch,
-                                  (batch.size(0)*2, 1, -1))
+                                  (batch.size(0)*2, 1, batch.size(2), batch.size(3)))
             # pass the batch through the model
             embeddings = model(batch)
             # reshape to the original (batch_size,2,fe)
             embeddings = torch.reshape(embeddings,
                                        (batch.size(0)//2, 2, -1))
             # split to e1 & e2 for each test pair
-            enrollment_embedding1, enrollment_embedding2 = torch.split(
-                embeddings, 1, dim=1)
-            # compute the cosine similarity
-            cossim = torch.nn.functional.cosine_similarity(enrollment_embedding1,
-                                                           enrollment_embedding2)
+            enrollment, verification = torch.split(embeddings,
+                                                   1,  # split_size
+                                                   dim=1)  # final 2* (batch_size,1,out_dim)
+            enrollment = torch.squeeze()  # reduce dim=1
+            verification = torch.squeeze()  # reduce dim=1
+            # compute the cosine similarity (batch_size,1)
+            cossim = torch.nn.functional.cosine_similarity(enrollment,
+                                                           verification)
 
             # batch and labels back to cpu
             batch.cpu()
             labels.cpu()
 
-            # calculating EER
-            diff = 1
-            EER = 0
-            EER_thresh = 0
-            EER_FAR = 0
-            EER_FRR = 0
+            # Fast EER computation
+            tunedThreshold, batch_EER, batch_EER_mean, fpr, fnr = tuneThresholdfromScore(cossim.cpu().detach(),
+                                                                                         labels.cpu(),
+                                                                                         [1, 0.1])
+            # eer=(far + frr)/2
+            batch_avg_EER_ = (batch_id * batch_avg_EER_ +
+                              batch_EER_mean)/(batch_id+1)
+            # eer=max(far+frr)
+            batch_avg_EER = (batch_id * batch_avg_EER + batch_EER)/(batch_id+1)
 
-            for thres in [0.01*i+0.5 for i in range(50)]:
-                # keep only values greater that threshold
-                sim_matrix_thresh = cossim > thres
+            logging.info(f"\nEER (epoch:{ e+1 }): {batch_avg_EER:.2f}")
+            logging.info(f"\navg_EER (epoch:{ e+1 }): {batch_avg_EER_:.2f}")
 
-                false_negatives = sum(
-                    [1 if (pred == 0 and lbl == 1) else 0 for pred, lbl in zip(batch, labels)])
-                false_positives = sum(
-                    [1 if (pred == 1 and lbl == 0) else 0 for pred, lbl in zip(batch, labels)])
-                n_aces = sum(labels)
-                n_zeros = len(labels) - n_aces
-                # false rejection rate (Type I Error)
-                FRR = false_negatives / n_aces
-                # false acceptance rate (Type II Error)
-                FAR = false_positives / n_zeros
+        # Get mean of #testing_epochs EER
+        avg_EER.append(batch_avg_EER)
+        avg_EER_.append(batch_avg_EER_)
 
-                # Save threshold when FAR = FRR (=EER)
-                if abs(FAR-FRR) < diff:
-                    diff = abs(FAR-FRR)
-                    EER = (FAR+FRR)/2
-                    EER_thresh = thres
-                    EER_FAR = FAR
-                    EER_FRR = FRR
-            batch_avg_EER += EER
-            logging.info("\nEER : %0.2f (thres:%0.2f, FAR:%0.2f, FRR:%0.2f)" %
-                         (EER, EER_thresh, EER_FAR, EER_FRR))
-        avg_EER += batch_avg_EER/(batch_id+1)
-    avg_EER = avg_EER / testing_epochs
-    print("\n EER across {0} epochs: {1:.4f}".format(testing_epochs, avg_EER))
+    print("\n EER across {0} epochs: {1:.4f} +- {2:.4f}".format(
+        testing_epochs, np.mean(avg_EER), np.std(avg_EER)))
+    print("\n avg_EER across {0} epochs: {1:.4f} +- {2:.4f}".format(
+        testing_epochs, np.mean(avg_EER_), np.std(avg_EER_)))
 
 
 def train_voxceleb():
@@ -271,8 +263,8 @@ def test_voxceleb(max_seq_len=245):
     test_loader = DataLoader(test_dataset, batch_size=config.BATCH_SIZE,
                              shuffle=True, num_workers=config.NUM_WORKERS, drop_last=False)
 
-    N, M, width, height = next(iter(test_loader)).size()
-
+    batch_, label_ = next(iter(test_loader)).size()
+    (N, M, width, height), label_size = batch_.size(), label_.size()
     # if your computer has a CUDA compatible gpu use it, otherwise use the cpu
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Selected Batch Size: {config.BATCH_SIZE}')

dataloading/voxceleb.py
@@ -113,6 +113,9 @@ class Voxceleb1_Evaluation_PreComputedMelSpectr(Dataset):
                  path_to_speakers='',
                  max_seq_len=None, fixed_length=True, fe_method="MEL_SPECTROGRAM",
                  *args, **kwargs):
+        self.fe_method = fe_method
+        self.fixed_length = fixed_length
+        self.max_seq_len = max_seq_len
         # Read the data from file
         with open(test_file_path, mode="r") as file:
             data_raw = file.readlines()
@@ -126,12 +129,13 @@ class Voxceleb1_Evaluation_PreComputedMelSpectr(Dataset):
         self.files1 = [os.path.join(path_to_test, f.strip()) for f in files1]
         self.files2 = [os.path.join(path_to_test, f.strip()) for f in files2]
         # convert to npy paths
-        self.files1 = list(map(lambda x: x[:-4]+'.npy', self.files1))
-        self.files2 = list(map(lambda x: x[:-4]+'.npy', self.files2))
+        self.files1 = list(map(lambda x: x[:-4]+'_mel.npy', self.files1))
+        self.files2 = list(map(lambda x: x[:-4]+'_mel.npy', self.files2))
         # make sure that all files exist and that none path is broken
         assert(all([os.path.exists(file) for file in self.files1+self.files2]))
         # typecasting str to integers
-        self.labels = list(map(int, labels))
+        self.labels = np.array(list(map(int, labels)), dtype=np.int8)
+        assert all([l in [0, 1] for l in labels])
 
     def __len__(self):
         return self.n_evaluations
@@ -143,7 +147,7 @@ class Voxceleb1_Evaluation_PreComputedMelSpectr(Dataset):
         features = self.zero_pad_and_stack([np.load(u) for u in [u1, u2]])
         # return 2 tensors
         # input_features(u1,u2) and label
-        return torch.Tensor(features), torch.Tensor(label)
+        return torch.tensor(features), torch.tensor(label)
 
     def zero_pad_and_stack(self, X):
         """