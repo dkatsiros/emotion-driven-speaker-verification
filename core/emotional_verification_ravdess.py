9489a53dc77cdbe9ca843f8b65d4187f6a7cb069
Dimitris Katsiros <dkatsiros@gmail.com>
2021-01-09 16:31:38 +0200

    Add main for emotional verification on ravdess, add dataset class

---
+++ core/emotional_verification_ravdess.py
@@ -0,0 +1,136 @@
+"""Speaker Verification for emotional content on voxceleb """
+# System imports
+import os
+import random
+import time
+import logging
+
+# External imports
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+import glob2 as glob
+from tqdm import tqdm
+
+# Relative imports
+import config
+from config import VARIABLES_FOLDER, RECOMPUTE, DETERMINISTIC
+from lib.metrics import tuneThresholdfromScore
+from lib.loss import GE2ELoss
+from lib.model_editing import drop_layers, print_require_grad_parameter
+from lib.sound_processing import compute_max_sequence_length, compute_sequence_length_distribution
+from lib.training import train_and_validate, test, results, overfit_batch
+from lib.training import deterministic_model
+from models.SpeechEmbedder import CNNSpeechEmbedder
+from dataloading.ravdess import RAVDESS_Evaluation_PreComputedMelSpectr
+
+
+def setup_environment_and_folders():
+    # Create all folders
+    dirs = [config.CHECKPOINT_FOLDER]
+    list(map(lambda x: os.makedirs(x, exist_ok=True), dirs))
+    deterministic_model(deterministic=config.DETERMINISTIC)
+
+
+def test_se(model, dataloader, testing_epochs=10):
+    """Implementing test for speaker verification. Metric average Equal Error Rate."""
+    # obtain the model's device ID
+    device = next(model.parameters()).device
+
+    avg_EER = []
+    avg_EER_ = []
+    for e in range(testing_epochs):
+        batch_avg_EER = 0
+        batch_avg_EER_ = 0
+        # dataloader: (batch_size,2,max_seq_len,input_fe)
+        for batch_id, (batch, labels) in enumerate(tqdm(dataloader)):
+            # move to device
+            batch = batch.to(device, dtype=torch.float)
+            labels = labels.to(device)
+            # prepare for forward pass
+            batch = torch.reshape(batch,
+                                  (batch.size(0)*2, 1, batch.size(2), batch.size(3)))
+            # pass the batch through the model
+            embeddings = model(batch)
+            # reshape to the original (batch_size,2,fe)
+            embeddings = torch.reshape(embeddings,
+                                       (batch.size(0)//2, 2, -1))
+            # split to e1 & e2 for each test pair
+            enrollment, verification = torch.split(embeddings,
+                                                   1,  # split_size
+                                                   dim=1)  # final 2* (batch_size,1,out_dim)
+            enrollment = torch.squeeze()  # reduce dim=1
+            verification = torch.squeeze()  # reduce dim=1
+            # compute the cosine similarity (batch_size,1)
+            cossim = torch.nn.functional.cosine_similarity(enrollment,
+                                                           verification)
+
+            # batch and labels back to cpu
+            batch.cpu()
+            labels.cpu()
+
+            # Fast EER computation
+            tunedThreshold, batch_EER, batch_EER_mean, fpr, fnr = tuneThresholdfromScore(cossim.cpu().detach(),
+                                                                                         labels.cpu(),
+                                                                                         [1, 0.1])
+            # eer=(far + frr)/2
+            batch_avg_EER_ = (batch_id * batch_avg_EER_ +
+                              batch_EER_mean)/(batch_id+1)
+            # eer=max(far+frr)
+            batch_avg_EER = (batch_id * batch_avg_EER + batch_EER)/(batch_id+1)
+
+            logging.info(f"\nEER (epoch:{ e+1 }): {batch_avg_EER:.2f}")
+            logging.info(f"\navg_EER (epoch:{ e+1 }): {batch_avg_EER_:.2f}")
+
+        # Get mean of #testing_epochs EER
+        avg_EER.append(batch_avg_EER)
+        avg_EER_.append(batch_avg_EER_)
+
+    print("\n EER across {0} epochs: {1:.4f} +- {2:.4f}".format(
+        testing_epochs, np.mean(avg_EER), np.std(avg_EER)))
+    print("\n avg_EER across {0} epochs: {1:.4f} +- {2:.4f}".format(
+        testing_epochs, np.mean(avg_EER_), np.std(avg_EER_)))
+
+
+def test_ravdess(max_seq_len=245):
+    """Load datasets, init models and test on VoxCeleb dataset."""
+
+    fe_method = "MEL_SPECTROGRAM" if config.CNN_BOOLEAN is True else "MFCC"
+    dataset_func = RAVDESS_Evaluation_PreComputedMelSpectr
+    # Test dataloader
+    test_dataset = dataset_func(test_file_path=config.TEST_FILE_PATH,
+                                fe_method=fe_method,
+                                max_seq_len=max_seq_len)
+    test_loader = DataLoader(test_dataset, batch_size=config.BATCH_SIZE,
+                             shuffle=True, num_workers=config.NUM_WORKERS, drop_last=False)
+
+    batch_, label_ = next(iter(test_loader)).size()
+    (N, M, width, height), label_size = batch_.size(), label_.size()
+    # if your computer has a CUDA compatible gpu use it, otherwise use the cpu
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f'Selected Batch Size: {config.BATCH_SIZE}')
+
+    # Restore trained model
+    try:
+        model = torch.load(config.MODEL_TO_RESTORE, map_location=device)
+    except Exception as model_can_not_be_restored:
+        raise FileNotFoundError from model_can_not_be_restored
+
+    # logging & parameters
+    logging.basicConfig(filename=config.LOG_FILE_TEST, level=logging.INFO)
+    print(f'Running on: {device}.\n')
+    # print(f'Model Parameters: {model.count_parameters()}')
+    print(model)
+
+    # Evaluation mode -gradients update off
+    model.eval()
+    # ===== TEST =====
+    test_se(model, test_loader, testing_epochs=50)
+
+
+if __name__ == "__main__":
+
+    # Set everthing up before starting training & testing
+    setup_environment_and_folders()
+    # Core
+    test_ravdess()
---
+++ dataloading/ravdess.py
@@ -0,0 +1,90 @@
+import os
+import numpy as np
+import glob2 as glob
+from random import shuffle
+from copy import deepcopy
+
+import torch
+from torch.utils.data import Dataset
+from imblearn.over_sampling import RandomOverSampler
+
+from utils import iemocap
+from lib import sound_processing
+from lib.sound_processing import load_wav, get_melspectrogram
+from lib.sound_processing import mfccs_and_spec
+from core.config import DATASET, FEATURE_EXTRACTOR
+from core import config
+
+
+class RAVDESS_Evaluation_PreComputedMelSpectr(Dataset):
+    def __init__(self, test_file_path="datasets/ravdess/veri_files/veri_test_exp1.1.txt",
+                 path_to_speakers='',
+                 max_seq_len=None, fixed_length=True, fe_method="MEL_SPECTROGRAM",
+                 *args, **kwargs):
+        self.fe_method = fe_method
+        self.fixed_length = fixed_length
+        self.max_seq_len = max_seq_len
+        # Read the data from file
+        with open(test_file_path, mode="r") as file:
+            data_raw = file.readlines()
+        data = [l.replace('\n', '').split() for l in data_raw]
+        # save length
+        self.n_evaluations = len(data)
+        # Get label,u1,u2
+        labels, files1, files2 = zip(*data)
+        # create absolute paths
+        path_to_test = 'datasets/ravdess/'
+        self.files1 = [os.path.join(path_to_test, f.strip()) for f in files1]
+        self.files2 = [os.path.join(path_to_test, f.strip()) for f in files2]
+        # convert to npy paths
+        self.files1 = list(map(lambda x: x[:-4]+'_mel.npy', self.files1))
+        self.files2 = list(map(lambda x: x[:-4]+'_mel.npy', self.files2))
+        # make sure that all files exist and that none path is broken
+        assert(all([os.path.exists(file) for file in self.files1+self.files2]))
+        # typecasting str to integers
+        self.labels = np.array(list(map(int, labels)), dtype=np.int8)
+        assert all([l in [0, 1] for l in labels])
+
+    def __len__(self):
+        return self.n_evaluations
+
+    def __getitem__(self, idx):
+
+        label, u1, u2 = self.labels[idx], self.files1[idx], self.files2[idx]
+        # load both utterances for npy precomputed format and zero pad
+        features = self.zero_pad_and_stack([np.load(u) for u in [u1, u2]])
+        # return 2 tensors
+        # input_features(u1,u2) and label
+        return torch.tensor(features), torch.tensor(label)
+
+    def zero_pad_and_stack(self, X):
+        """
+        This function performs zero padding on a list of features and forms them into a numpy 3D array
+
+        Returns:
+            padded: a 3D numpy array of shape num_sequences x max_sequence_length x feature_dimension
+        """
+
+        if self.fe_method == "MEL_SPECTROGRAM":
+            max_length = self.max_seq_len
+
+            feature_dim = X[0].shape[-1]
+            padded = np.zeros((len(X), max_length, feature_dim))
+
+            # Do the actual work
+            for i in range(len(X)):
+                if X[i].shape[0] < max_length:
+                    # Needs padding
+                    diff = max_length - X[i].shape[0]
+                    # pad
+                    X[i] = np.vstack((X[i], np.zeros((diff, feature_dim))))
+                else:
+                    if self.fixed_length is True:
+                        # Set a fixed length => information loss
+                        X[i] = np.take(X[i], list(
+                            range(0, max_length)), axis=0)
+                # Add to padded
+                padded[i, :, :] = X[i]
+            return padded
+        raise NotImplementedError(
+            "Zero padding works only with mel spectrogram for now")
