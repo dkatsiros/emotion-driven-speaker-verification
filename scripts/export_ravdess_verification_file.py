db888572df5e3e4ee6ded54269d798bf69865541
Dimitris <dkatsiros@gmail.com>
2021-01-10 22:29:43 +0200

    Update pipeline to numpy idx, add balanced positive and negative pairs

--- scripts/export_ravdess_verification_file.py
+++ scripts/export_ravdess_verification_file.py
@@ -1,6 +1,8 @@
 """Create a test file for speaker verification using RAVDESS dataset"""
+import itertools
 import os
 from random import shuffle
+import numpy as np
 from utils.load_dataset import load_RAVDESS
 
 
@@ -9,7 +11,7 @@ def export_verification_file(pairs=None, path='./temp.txt'):
         for pair in pairs:
             label, u1, u2 = pair
             file.write(f"{label} {u1} {u2}\n")
-    print(f'Export finished for {path}')
+    print(f'Export finished for {path} with {len(pairs)} pairs.')
 
 
 # Load dataset where each label is a list containing
@@ -25,8 +27,33 @@ X = [x.replace("datasets/ravdess/", "") for x in X]
 # 4 = angry, 5 = fearful, 6 = disgust, 7 = surprised
 emotion = [int(l[2])-1 for l in y]
 emotional_intensity = [int(l[3])-1 for l in y]  # 0 normal, 1 strong
-speaker_id = [int(l[-1])-1 for l in y]  # 0 to 23 (12 men 12 women)
+speaker_id = [int(l[-1])-1 for l in y]  # 0,1,2,...,23 (12 men 12 women)
+statement = [int(l[4])-1 for l in y]  # 0 or 1
+repetition = [int(l[5])-1 for l in y]  # 0 or 1
 
+# Variables aligned with matrix dimensions
+SPEAKERS = 24
+EMOTIONS = 8
+STATEMENTS = 2
+INTENSITIES = 2
+REPETITIONS = 2
+
+
+# Create an indexing list for emotions
+idx = np.zeros((SPEAKERS, EMOTIONS, STATEMENTS,
+                INTENSITIES, REPETITIONS), dtype=int)
+
+# Inverse mapping
+x_contains = [(speaker_id[i], emotion[i], statement[i], emotional_intensity[i], repetition[i])
+              for i in range(len(X))]
+
+# invert to get X[ idx[a,b,c,d] ] = sample
+for i in range(len(X)):
+    sp, em, st, inte, rep = x_contains[i]
+    idx[sp, em, st, inte, rep] = i
+
+# print(idx[23, 7, 1, 1, 1])
+# exit()
 # INITIALIZE
 # neutral samples
 X_neutral = []
@@ -62,39 +89,50 @@ for x, emotion, intensity, spkr in zip(X, emotion, emotional_intensity, speaker_
 # a list of tuples (label,utterance_1,utterance2)
 # which we will evaluate during test time for verification
 pairs_norm = []
-for idx_neutral in range(len(X_neutral)):
-    for idx_norm in range(len(X_norm)):
-        # Get label for speaker verification if same speakers
-        if speaker_id_norm[idx_norm] == speaker_id_neutral[idx_neutral]:
-            label = 1
-        else:
-            label = 0
-        # actual samples
-        u1 = X_norm[idx_norm]
-        u2 = X_neutral[idx_neutral]
-        # save pairs for exporting into file
-        pair = (label, u1, u2)
-        pairs_norm.append(pair)
+# Outer product to reduce time
+for sp, em, st, rep in itertools.product(range(SPEAKERS),
+                                         range(1, EMOTIONS),
+                                         range(STATEMENTS),
+                                         range(REPETITIONS)):
+    # Normal-emotionally enrollment utterance
+    enrollment = X[idx[sp, em, st, 0, rep]]  # with emotion
+    verification = X[idx[sp, 0, st, 0, rep]]  # no emotion, no intens
+    # add pair with the same speaker, so label=1
+    pairs_norm.append((1, enrollment, verification))
 
-# STRONG EMOTION IN ENROLLMENT UTTERANCE
-# VERIFICATION UTTERANCE IS "EMOTION FREE"
-# a list of tuples (label,utterance_1,utterance2)
-# which we will evaluate during test time for verification
+    # create a list without `sp` speaker id to pick from
+    left_speakers = list(range(0, sp)) + list(range(sp+1, SPEAKERS))
+    # add a different speaker (label=0)
+    diff_sp = int(np.random.choice(left_speakers, 1))
+    # Normal-emotionally enrollment utterance
+    # but this time from another speaker
+    enrollment = X[idx[diff_sp, em, st, 0, rep]]  # with emotion
+    verification = X[idx[sp, 0, st, 0, rep]]  # no emotion,intens
+    # same speaker so label=1
+    pairs_norm.append((0, enrollment, verification))
+
 pairs_strong = []
-for idx_neutral in range(len(X_neutral)):
-    for idx_strong in range(len(X_strong)):
-        # Get label for speaker verification if same speakers
-        if speaker_id_strong[idx_strong] == speaker_id_neutral[idx_neutral]:
-            label = 1
-        else:
-            label = 0
-        # enrollment utterance
-        u1 = X_strong[idx_strong]
-        # verification utterance
-        u2 = X_neutral[idx_neutral]
-        # save pairs for exporting into file
-        pair = (label, u1, u2)
-        pairs_strong.append(pair)
+# Outer product to reduce time
+for sp, em, st, rep in itertools.product(range(SPEAKERS),
+                                         range(1, EMOTIONS),
+                                         range(STATEMENTS),
+                                         range(REPETITIONS)):
+    # Normal-emotionally enrollment utterance
+    enrollment = X[idx[sp, em, st, 1, rep]]  # with emotion
+    verification = X[idx[sp, 0, st, 0, rep]]  # no emotion, no intens
+    # add pair with the same speaker, so label=1
+    pairs_strong.append((1, enrollment, verification))
+
+    # create a list without `sp` speaker id to pick from
+    left_speakers = list(range(0, sp)) + list(range(sp+1, SPEAKERS))
+    # add a different speaker (label=0)
+    diff_sp = int(np.random.choice(left_speakers, 1))
+    # Normal-emotionally enrollment utterance
+    # but this time from another speaker
+    enrollment = X[idx[diff_sp, em, st, 1, rep]]  # with emotion
+    verification = X[idx[sp, 0, st, 0, rep]]  # no emotion,intens
+    # same speaker so label=1
+    pairs_strong.append((0, enrollment, verification))
 
 # shuffle
 shuffle(pairs_norm)
@@ -105,5 +143,5 @@ os.makedirs('datasets/ravdess/veri_files/', exist_ok=True)
 export_verification_file(pairs=pairs_norm,
                          path="datasets/ravdess/veri_files/veri_test_exp1.1.txt")
 
-export_verification_file(pairs=pairs_norm,
+export_verification_file(pairs=pairs_strong,
                          path="datasets/ravdess/veri_files/veri_test_exp1.2.txt")
