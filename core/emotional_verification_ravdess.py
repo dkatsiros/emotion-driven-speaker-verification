Fix EER averaging, add baselines for relative values in ravdess experiments

core/emotional_verification_ravdess.py
@@ -32,13 +32,15 @@ def setup_environment_and_folders():
     deterministic_model(deterministic=config.DETERMINISTIC)
 
 
-def test_se(model, dataloader, testing_epochs=10):
+def test_se(model, dataloader, testing_epochs=1):
     """Implementing test for speaker verification. Metric average Equal Error Rate."""
     # obtain the model's device ID
     device = next(model.parameters()).device
 
     avg_EER = []
-    avg_mindcf = []
+    all_cossim = []
+    all_labels = []
+    # avg_mindcf = []
     for e in range(testing_epochs):
         epoch_avg_EER = 0
         batch_mindcf = 0
@@ -69,37 +71,45 @@ def test_se(model, dataloader, testing_epochs=10):
             batch.cpu()
             labels.cpu()
 
-            # Fast EER computation
-            tunedThreshold, batch_EER, fpr, fnr = tuneThresholdfromScore(cossim.cpu().detach(),
-                                                                         labels.cpu(),
-                                                                         [1, 0.1])
-            fnrs, fprs, thresholds = ComputeErrorRates(cossim.cpu().detach(),
-                                                       labels.cpu())
-            # eer=(far + frr)/2
-            epoch_avg_EER = (batch_id * epoch_avg_EER +
-                             batch_EER)/(batch_id+1)
+            # total computation
+            all_cossim.extend(cossim.cpu().detach())
+            all_labels.extend(labels.cpu())
 
-            p_target = 0.01
-            c_miss = 1
-            c_fa = 1
+            # # Fast EER computation
+            # tunedThreshold, batch_EER, fpr, fnr = tuneThresholdfromScore(cossim.cpu().detach(),
+            #                                                              labels.cpu(),
+            #                                                              [1, 0.1])
+            # # fnrs, fprs, thresholds = ComputeErrorRates(cossim.cpu().detach(),
+            # #                                            labels.cpu())
+            # # eer=(far + frr)/2
+            # epoch_avg_EER = (batch_id * epoch_avg_EER +
+            #                  batch_EER)/(batch_id+1)
 
-            mindcf, _ = ComputeMinDcf(fnrs, fprs, thresholds,
-                                      p_target, c_miss, c_fa)
-            batch_mindcf = (batch_id * batch_mindcf + mindcf)/(batch_id+1)
+            # p_target = 0.01
+            # c_miss = 1
+            # c_fa = 1
 
-            logging.info(f"\navg_EER (epoch:{ e+1 }): {epoch_avg_EER:.2f}")
-            logging.info(f"\nmin DCF (epoch: {e+1}): {mindcf:.2f}")
+            # mindcf, _ = ComputeMinDcf(fnrs, fprs, thresholds,
+            #                           p_target, c_miss, c_fa)
+            # batch_mindcf = (batch_id * batch_mindcf + mindcf)/(batch_id+1)
+
+            # logging.info(f"\navg_EER (epoch:{ e+1 }): {epoch_avg_EER:.2f}")
+            # logging.info(f"\nmin DCF (epoch: {e+1}): {mindcf:.2f}")
 
         # Get mean of #testing_epochs EER
-        avg_EER.append(epoch_avg_EER)
-        avg_mindcf.append(batch_mindcf)
+        # avg_EER.append(epoch_avg_EER)
+        # avg_mindcf.append(batch_mindcf)
+        tunedThreshold, EER, fpr, fnr = tuneThresholdfromScore(all_cossim,
+                                                                all_labels,
+                                                                [1, 0.1])
+        avg_EER.append(EER)
+        print(f"\navg_EER (epoch:{ e+1 }): {EER:.2f}")
+        logging.info(f"\navg_EER (epoch:{ e+1 }): {epoch_avg_EER:.2f}")
 
-    print("\n avg_EER across {0} epochs: {1:.2f} \pm {2:.2f}".format(
-        testing_epochs, np.mean(avg_EER), np.std(avg_EER)))
-    print("\n min_dcf across {0} epochs: {1:.2f} \pm {2:.2f}".format(
-        testing_epochs, np.mean(avg_mindcf), np.std(avg_mindcf)))
-    return (round(np.mean(avg_EER), 2), round(np.std(avg_EER), 2),
-            round(np.mean(avg_mindcf), 4), round(np.std(avg_mindcf), 4))
+    # print("\n min_dcf across {0} epochs: {1:.2f} \pm {2:.2f}".format(
+    #     testing_epochs, np.mean(avg_mindcf), np.std(avg_mindcf)))
+    return (round(EER, 2),0,
+            0,0)
 
 
 def test_ravdess(model=config.MODEL_TO_RESTORE,
@@ -138,7 +148,7 @@ def test_ravdess(model=config.MODEL_TO_RESTORE,
     model.eval()
     # ===== TEST =====
     mean_eer, std_eer, mean_dcf, std_dcf = test_se(
-        model, test_loader, testing_epochs=50)
+        model, test_loader, testing_epochs=1)
     return mean_eer, std_eer, mean_dcf, std_dcf
 
 
@@ -248,10 +258,13 @@ the results to a file
         file.write(
             f"{model}\t{verification_file}\t{mean_eer}\t{std_eer}\t{mean_dcf}\t{std_dcf}\n")
 
-
 def export_latex(ifile=None, ofile=None):
     if ifile is None:
         raise AssertionError()
+    # get values from baseline file
+    baseline_values = [[11.46, 15.62, 15.1, 19.27, 20.31, 22.92, 18.23, 20.31],
+                       [14.58, 35.42, 25.0, 44.79, 34.38, 25.0, 23.96]]
+
     with open(ifile, mode="r") as file:
         data = file.readlines()
     exp_results = []
@@ -261,12 +274,12 @@ def export_latex(ifile=None, ofile=None):
         exp_results.append(
             (model, verification_file, mean_eer, std_eer, mean_dcf, std_dcf))
     if ofile is None:
-        ofile = ifile[:-3] + '.tex'
-    HEADER = """\\begin{tabular}{ |p{0.8cm}|p{6cm}||p{2cm}|p{2cm}| }
+        ofile = ifile[:-3] + '_plus_relative' + '.tex'
+    HEADER = """\\begin{tabular}{ |p{0.8cm}|p{2cm}||p{2.5cm}|p{2cm}|p{2.5cm}|p{2cm}| }
  \hline
- \multicolumn{4}{|c|}{Neutral enrollment and \\textit{weak or strong} emotionally injected verification utterance} \\\\
+ \multicolumn{6}{|c|}{Neutral enrollment and \\textit{weak or strong} emotionally injected verification utterance} \\\\
  \hline
- Exp & emotion & weak & strong \\\\
+ Exp & emotion & weak & relative & strong & relative \\\\
  \hline
 """
     emotion_names = ["neutral", "calm", "happy", "sad",
@@ -276,23 +289,49 @@ def export_latex(ifile=None, ofile=None):
         file.write(HEADER)
         eer_weak = []
         eer_strong = []
-        for data, data_intense in zip(exp_results[::2], exp_results[1::2]):
+        relative_weak = []
+        relative_strong = []
+        for (data, data_intense,
+             baseline, baseline_intense) in zip(exp_results[::2],
+                                                exp_results[1::2],
+                                                baseline_values[0],
+                                                baseline_values[1]):
             # Weak emotion
             model, verification_file, mean_eer, std_eer, mean_dcf, std_dcf = data
-            eer_weak.append(mean_eer)
+            eer_weak.append(float(mean_eer))
             exp_details = verification_file[-9:][:-4]
             exp_num, intensity, emotion = exp_details.split('.')
-            result1 = "\SI{"+mean_eer + "\pm" + std_eer + "}"
+            result1 = "\SI{"+mean_eer  + "}"
+            relative_value_1 = round(
+                ((baseline - float(mean_eer)) / baseline * 100), 2)
+            relative_weak.append(relative_value_1)
+            relative1 = f"{relative_value_1} \%"
             # Strong emotion
             model, verification_file, mean_eer, std_eer, mean_dcf, std_dcf = data_intense
-            eer_strong.append(mean_eer)
+            eer_strong.append(float(mean_eer))
             exp_details = verification_file[-9:][:-4]
             exp_num, intensity, emotion = exp_details.split('.')
-            result2 = "\SI{"+mean_eer + "\pm" + std_eer + "}"
+            result2 = "\SI{"+mean_eer + "}"
+            relative_value_2 = round(
+                (baseline_intense - float(mean_eer)) / baseline_intense * 100, 2)
+            relative_strong.append(relative_value_2)
+            relative2 = f"{relative_value_2} \%"
             # write file
             file.write(
-                f""" {exp_num}.{emotion} & {emotion_names[int(emotion)]}  & ${result1}$ & ${result2}$\\\\\n""")
+                f""" {exp_num}.{emotion} & {emotion_names[int(emotion)]}  & ${result1}$ & ${relative1}$ & ${result2}$ & ${relative2}$\\\\\n""")
         file.write("\hline")
+        mean_1 = round(np.mean(eer_weak), 2)
+        mean_2 = round(np.mean(relative_weak), 2)
+        mean_3 = round(np.mean(eer_strong), 2)
+        mean_4 = round(np.mean(relative_strong), 2)
+        baseline_mean_weak = round(np.mean(baseline_values[0]), 2)
+        baseline_mean_strong = round(np.mean(baseline_values[1]), 2)
+        diff_weak = round((baseline_mean_weak - mean_1) /
+                          baseline_mean_weak * 100, 2)
+        diff_strong = round(
+            (baseline_mean_strong - mean_3) / baseline_mean_strong * 100, 2)
+        file.write(
+            f"""\n3.8 & average & {mean_1} & {diff_weak:+}\% \% & {mean_3}  & {diff_strong:+}\% \% \\\\""")
         file.write(""" \hline
  \end{tabular} \\break\\break\\break
 """)
@@ -307,6 +346,10 @@ def exp4_export_latex(model=None, ofile=None):
     if not os.path.exists(ifile_knowledge):
         raise FileNotFoundError()
 
+    relative_improvement_array = []
+    # store baseline model values
+    baseline_values = [[16.67, 25.0, 17.45, 33.59, 30.99, 25.0, 22.66],
+                       [8.48, 18.82, 19.79, 27.53, 27.98, 19.87, 16.37]]
     # store results
     exp_results_ignorance = []
     with open(ifile_ignorance, mode="r") as file:
@@ -332,11 +375,11 @@ def exp4_export_latex(model=None, ofile=None):
     if ofile is None:
         ofile = f"results/results_exp4.{model[:-3]}.tex"
 
-    HEADER = """\\begin{tabular}{ |p{0.8cm}|p{2cm}||p{2cm}|p{2cm}| }
+    HEADER = """\\begin{tabular}{ |p{0.8cm}|p{2cm}||p{2.5cm}|p{2.5cm}|p{2cm}| }
  \hline
- \multicolumn{4}{|c|}{Emotion ignorance vs emotion knowledge for model } \\\\
+ \multicolumn{5}{|c|}{Emotion ignorance vs emotion knowledge for model } \\\\
  \hline
- Exp & emotion & ignorance & knowledge \\\\
+ Exp & emotion & ignorance & knowledge & relative \\\\
  \hline
 """
     emotion_names = ["neutral", "calm", "happy", "sad",
@@ -346,27 +389,40 @@ def exp4_export_latex(model=None, ofile=None):
         file.write(HEADER)
         for idx, (ignorance, knowledge) in enumerate(zip(exp_results_ignorance, exp_results_knowledge), 1):
             # ignorance
-            model, verification_file, mean_eer, std_eer, mean_dcf, std_dcf = ignorance
+            model, verification_file, mean_eer_ignorance, std_eer, mean_dcf, std_dcf = ignorance
             # exp_details = verification_file[-9:][:-4]
             # exp_num, intensity, emotion = exp_details.split('.')
-            result1 = "\SI{"+mean_eer + "\pm" + std_eer + "}"
+            result1 = "\SI{"+mean_eer_ignorance + "}"
             # Strong emotion
-            model, verification_file, mean_eer, std_eer, mean_dcf, std_dcf = knowledge
+            model, verification_file, mean_eer_knowledge, std_eer, mean_dcf, std_dcf = knowledge
             # exp_details = verification_file[-9:][:-4]
             # exp_num, intensity, emotion = exp_details.split('.')
-            result2 = "\SI{"+mean_eer + "\pm" + std_eer + "}"
+            result2 = "\SI{"+mean_eer_knowledge + "}"
+            relative_improvement = round(
+                (float(mean_eer_ignorance) - float(mean_eer_knowledge)) / float(mean_eer_ignorance) * 100, 2)
+            relative_improvement_array.append(relative_improvement)
+            result3 = f"{relative_improvement} \%"
             # write file
             file.write(
-                f""" 4.{idx} & {emotion_names[int(idx)]}  & ${result1}$ & ${result2}$\\\\\n""")
+                f""" 4.{idx} & {emotion_names[int(idx)]}  & ${result1}$ & ${result2}$ & ${result3}$\\\\\n""")
         # mean EER
         avg_ignorance = np.mean([float(x[2]) for x in exp_results_ignorance])
         avg_knowledge = np.mean([float(x[2]) for x in exp_results_knowledge])
+        avg_baseline_ignorance = np.mean(baseline_values[0])
+        avg_baseline_knowledge = np.mean(baseline_values[1])
+        avg_relative_baseline_impr_ignorance = round((
+            avg_baseline_ignorance - avg_ignorance) / avg_baseline_ignorance * 100, 2)
+        avg_relative_baseline_impr_knowledge = round((
+            avg_baseline_knowledge - avg_knowledge) / avg_baseline_knowledge * 100, 2)
+        avg_relative_improvement = round(
+            np.mean(relative_improvement_array), 2)
         file.write("\hline")
         file.write(
-            f""" 4.{len(exp_results_ignorance)+1} & average  & ${avg_ignorance:.2f}$ & ${avg_knowledge:.2f}$\\\\\n""")
+            f""" 4.{len(exp_results_ignorance)+1} & average  & ${avg_ignorance:.2f}\ ({avg_relative_baseline_impr_ignorance:+} \%)$ & ${avg_knowledge:.2f}\ ({avg_relative_baseline_impr_knowledge:+} \%)$ & ${avg_relative_improvement} \%$\\\\\n""")
         file.write(""" \hline
  \end{tabular} \\break\\break\\break
 """)
+
 
 
 if __name__ == "__main__":
@@ -376,20 +432,50 @@ if __name__ == "__main__":
     # Core
     # mean_eer, std_eer, mean_dcf, std_dcf = test_ravdess()
 
-    # export_visual_representation_2D(
-    #     ofile="datasets/ravdess/representation_exp2.1.npy")
 
-    # visualize("datasets/ravdess/representation_exp1.1.npy",
-    #           "datasets/ravdess/latent_space_exp1.1.png")
-    model = "checkpoints/emot_frozenconv_voxceleb_lr=.5e-1.pt"
+    # ## EXPERIMENT 1 AND 2
+    model = "checkpoints/emot_vox_ver2_lr=1e-1.pt"
+    model = model.replace("checkpoints/","")
+    for exp_num, intensity in [[1,1],[1,2],[2,1],[2,2]]:
+        test_ravdess_and_export(model=f"checkpoints/{model}",
+                                verification_file=f"datasets/ravdess/veri_files/veri_test_exp{exp_num}.{intensity}.txt",
+                                ofile=f"results/exp{exp_num}.{model}")
+
+    # # export_visual_representation_2D(
+    # #     ofile="datasets/ravdess/representation_exp2.1.npy")
+
+    # # visualize("datasets/ravdess/representation_exp1.1.npy",
+    # #           "datasets/ravdess/latent_space_exp1.1.png")
+    # # model = "checkpoints/emot_iemocap_complex_cl=4_lr=1e-3.pt"
+    # # model = "checkpoints/fusion_model_lr=1e-3.pt"
+    # # model="checkpoints/emot_frozen1stconv_voxceleb_lr=1e-2.pt"
+
+    # EXPERIMENT 3
+    model = "checkpoints/" + model
     ofile = f"results/exp3.{model.split('/')[-1][:-3]}.txt"
     for emotion in [1, 2, 3, 4, 5, 6, 7]:
         for intensity in [1, 2]:
             verification_file = f"datasets/ravdess/veri_files/veri_test_exp3.{intensity}.{emotion}.txt"
+            # exp4 only
+            ofile = f"results/exp4.{intensity}.{model.split('/')[-1][:-3]}.txt"
             test_ravdess_and_export(model=model,
                                     verification_file=verification_file,
                                     ofile=ofile)
 
-    # export_latex(ifile=ofile)
-    # exp4_export_latex(model="emot_frozen1stconv_voxceleb_lr=1e-2.pt")
-#
+    export_latex(ifile=ofile)
+
+    # EXPERIMENT 4
+    ofile = f"results/exp4.{model.split('/')[-1][:-3]}.txt"
+    for emotion in [1, 2, 3, 4, 5, 6, 7]:
+        for intensity in [1, 2]:
+            verification_file = f"datasets/ravdess/veri_files/veri_test_exp4.{intensity}.{emotion}.txt"
+            # exp4 only
+            ofile = f"results/exp4.{intensity}.{model.split('/')[-1][:-3]}.txt"
+            test_ravdess_and_export(model=model,
+                                    verification_file=verification_file,
+                                    ofile=ofile)
+
+    model = model.replace("checkpoints/","")
+    exp4_export_latex(model=model)
+# # #
+    print(f"finished testing model: {model}.")

core/main_voxceleb.py
@@ -111,6 +111,8 @@ def test_se(model, dataloader, testing_epochs=10):
 
     avg_EER = []
     avg_mindcf = []
+    all_cossim = []
+    all_labels = []
     for e in range(testing_epochs):
         epoch_avg_EER = 0
         batch_mindcf = 0
@@ -140,35 +142,44 @@ def test_se(model, dataloader, testing_epochs=10):
             # batch and labels back to cpu
             batch.cpu()
             labels.cpu()
-
-            tunedThreshold, batch_EER, fpr, fnr = tuneThresholdfromScore(cossim.cpu().detach(),
-                                                                         labels.cpu(),
-                                                                         [1, 0.1])
-            fnrs, fprs, thresholds = ComputeErrorRates(cossim.cpu().detach(),
-                                                       labels.cpu())
+            
+            # total computation
+            all_cossim.extend(cossim.cpu().detach())
+            all_labels.extend(labels.cpu())
+            # tunedThreshold, batch_EER, fpr, fnr = tuneThresholdfromScore(cossim.cpu().detach(),
+            #                                                              labels.cpu(),
+            #                                                              [1, 0.1])
+            # fnrs, fprs, thresholds = ComputeErrorRates(cossim.cpu().detach(),
+            #                                            labels.cpu())
             # eer=(far + frr)/2
-            epoch_avg_EER = (batch_id * epoch_avg_EER +
-                             batch_EER)/(batch_id+1)
+            # epoch_avg_EER = (batch_id * epoch_avg_EER +
+            #                  batch_EER)/(batch_id+1)
 
-            p_target = 0.01
-            c_miss = 1
-            c_fa = 1
+            # p_target = 0.01
+            # c_miss = 1
+            # c_fa = 1
 
-            mindcf, _ = ComputeMinDcf(fnrs, fprs, thresholds,
-                                      p_target, c_miss, c_fa)
-            batch_mindcf = (batch_id * batch_mindcf + mindcf)/(batch_id+1)
+            # mindcf, _ = ComputeMinDcf(fnrs, fprs, thresholds,
+            #                           p_target, c_miss, c_fa)
+            # batch_mindcf = (batch_id * batch_mindcf + mindcf)/(batch_id+1)
 
-            logging.info(f"\navg_EER (epoch:{ e+1 }): {epoch_avg_EER:.2f}")
-            logging.info(f"\nmin DCF (epoch: {e+1}): {mindcf:.2f}")
+            # logging.info(f"\navg_EER (epoch:{ e+1 }): {epoch_avg_EER:.2f}")
+            # logging.info(f"\nmin DCF (epoch: {e+1}): {mindcf:.2f}")
 
         # Get mean of #testing_epochs EER
-        avg_EER.append(epoch_avg_EER)
-        avg_mindcf.append(batch_mindcf)
+        # avg_EER.append(epoch_avg_EER)
+        # avg_mindcf.append(batch_mindcf)
+        tunedThreshold, EER, fpr, fnr = tuneThresholdfromScore(all_cossim,
+                                                                all_labels,
+                                                                [1, 0.1])
 
-    print("\n avg_EER across {0} epochs: {1:.4f} +- {2:.4f}".format(
-        testing_epochs, np.mean(avg_EER), np.std(avg_EER)))
-    print("\n min_dcf across {0} epochs: {1:.4f} +- {2:.4f}".format(
-        testing_epochs, np.mean(avg_mindcf), np.std(avg_mindcf)))
+        avg_EER.append(EER)
+        print(f"\navg_EER (epoch:{ e+1 }): {EER:.2f}")
+        logging.info(f"\navg_EER (epoch:{ e+1 }): {epoch_avg_EER:.2f}")
+    # print("\n avg_EER across {0} epochs: {1:.4f} +- {2:.4f}".format(
+    #     testing_epochs, np.mean(avg_EER), np.std(avg_EER)))
+    # print("\n min_dcf across {0} epochs: {1:.4f} +- {2:.4f}".format(
+    #     testing_epochs, np.mean(avg_mindcf), np.std(avg_mindcf)))
 
 
 def train_voxceleb():
@@ -293,7 +304,7 @@ def test_voxceleb(max_seq_len=245):
     # Evaluation mode -gradients update off
     model.eval()
     # ===== TEST =====
-    test_se(model, test_loader, testing_epochs=50)
+    test_se(model, test_loader, testing_epochs=1)
 
 
 if __name__ == "__main__":