8dde2f3edaa458cafab03e57027de39543c21a65
Dimitris <dkatsiros@gmail.com>
2021-04-07 18:04:12 +0300

    Fix missing .wav in path

--- utils/crema_d.py
+++ utils/crema_d.py
@@ -336,17 +336,17 @@ def load_CREMAD_SER(n_emotions=4,
     # and keep only n_emotions
     # train
     train_pairs = import_csv(filepath=train_filepath)
-    X_train, y_train = zip(*[(os.path.join(dataset_path, "AudioWAV", x[0]),
+    X_train, y_train = zip(*[(os.path.join(dataset_path, "AudioWAV", x[0] + ".wav"),
                               x[3])
                              for x in train_pairs if x[3] < n_emotions])
     # test
     test_pairs = import_csv(filepath=test_filepath)
-    X_test, y_test = zip(*[(os.path.join(dataset_path, "AudioWAV", x[0]),
+    X_test, y_test = zip(*[(os.path.join(dataset_path, "AudioWAV", x[0] + ".wav"),
                             x[3])
                            for x in test_pairs if x[3] < n_emotions])
     # validation
     val_pairs = import_csv(filepath=val_filepath)
-    X_val, y_val = zip(*[(os.path.join(dataset_path, "AudioWAV", x[0]),
+    X_val, y_val = zip(*[(os.path.join(dataset_path, "AudioWAV", x[0] + ".wav"),
                           x[3])
                          for x in val_pairs if x[3] < n_emotions])
 
